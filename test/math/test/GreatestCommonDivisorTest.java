2d8a9d777417414a7d219b55a55dae8d97553e8b
Priscilla Romagnoli <priscillaromagnoli@hotmail.com>
2018-07-04 22:04:22 -0300

    Copied GCD algorithm

---
+++ test/math/test/GreatestCommonDivisorTest.java
@@ -0,0 +1,21 @@
+package math.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GreatestCommonDivisorTest {
+    @Test
+    public void oneAndOne() {
+        assertEquals(1, gcd(1, 1));
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % t;
+            a = t;
+        }
+        return a;
+    }
+}
---
+++ test/math/test/ReduceFractionTest.java
@@ -0,0 +1,20 @@
+package math.test;
+
+import math.Fraction;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReduceFractionTest {
+    @Test
+    public void alreadyInLowestTerms() {
+        assertEquals(new Fraction(3, 4), new Fraction(3, 4));
+    }
+
+    @Test
+    @Ignore("Waiting To Implement GCD")
+    public void reduceNotToWholeNumber() {
+        assertEquals(new Fraction(3, 4), new Fraction(6, 8));
+    }
+}
