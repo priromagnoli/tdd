1fe4f447b75db846e6113c0b9b87872ebf7567b6
Priscilla Romagnoli <priscillaromagnoli@hotmail.com>
2018-07-06 14:23:06 -0300

    Making CGD always positive

--- test/math/test/GreatestCommonDivisorTest.java
+++ test/math/test/GreatestCommonDivisorTest.java
@@ -9,14 +9,14 @@ public class GreatestCommonDivisorTest {
     public void reflexive() {
         assertEquals(1, gcd(1, 1));
         assertEquals(2, gcd(2, 2));
-        assertEquals(-1, gcd(-1, -1));
+        assertEquals(1, gcd(-1, -1));
     }
 
     @Test
     public void relativelyPrime() {
         assertEquals(1, gcd(2, 3));
         assertEquals(1, gcd(4, 7));
-        assertEquals(-1, gcd(-2, -3));
+        assertEquals(1, gcd(-2, -3));
     }
 
     @Test
@@ -29,13 +29,13 @@ public class GreatestCommonDivisorTest {
     public void commonFactor() {
         assertEquals(2, gcd(6, 8));
         assertEquals(7, gcd(49, 315));
-        assertEquals(-4, gcd(-24, -28));
+        assertEquals(4, gcd(-24, -28));
     }
 
     @Test
     public void negatives() {
         assertEquals(4, gcd(-24, 28));
-        assertEquals(-4, gcd(24, -28));
+        assertEquals(4, gcd(24, -28));
     }
 
     private int gcd(int a, int b) {
@@ -44,6 +44,6 @@ public class GreatestCommonDivisorTest {
             b = a % t;
             a = t;
         }
-        return a;
+        return Math.abs(a);
     }
 }
