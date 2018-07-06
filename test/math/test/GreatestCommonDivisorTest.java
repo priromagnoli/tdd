1046d6be389eb219d5a02d6e4d1bab52d1fb6e98
Priscilla Romagnoli <priscillaromagnoli@hotmail.com>
2018-07-06 14:18:21 -0300

    Made tests for GCD

--- test/math/test/GreatestCommonDivisorTest.java
+++ test/math/test/GreatestCommonDivisorTest.java
@@ -6,8 +6,36 @@ import static org.junit.Assert.assertEquals;
 
 public class GreatestCommonDivisorTest {
     @Test
-    public void oneAndOne() {
+    public void reflexive() {
         assertEquals(1, gcd(1, 1));
+        assertEquals(2, gcd(2, 2));
+        assertEquals(-1, gcd(-1, -1));
+    }
+
+    @Test
+    public void relativelyPrime() {
+        assertEquals(1, gcd(2, 3));
+        assertEquals(1, gcd(4, 7));
+        assertEquals(-1, gcd(-2, -3));
+    }
+
+    @Test
+    public void oneIsMultipleOfTheOther() {
+        assertEquals(3, gcd(3, 9));
+        assertEquals(5, gcd(5, 30));
+    }
+
+    @Test
+    public void commonFactor() {
+        assertEquals(2, gcd(6, 8));
+        assertEquals(7, gcd(49, 315));
+        assertEquals(-4, gcd(-24, -28));
+    }
+
+    @Test
+    public void negatives() {
+        assertEquals(4, gcd(-24, 28));
+        assertEquals(-4, gcd(24, -28));
     }
 
     private int gcd(int a, int b) {
