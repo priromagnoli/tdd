451e58890d750a55c0766eadc3a37c1a550ae233
Priscilla Romagnoli <priscillaromagnoli@hotmail.com>
2018-07-08 20:01:22 -0300

    More examples for reducing

--- test/math/test/AddFractionsTest.java
+++ test/math/test/AddFractionsTest.java
@@ -1,6 +1,7 @@
 package math.test;
 
 import math.Fraction;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -45,5 +46,37 @@ public class AddFractionsTest {
     @Test
     public void differentDenominatorsWithoutReducing() {
         assertEquals(new Fraction(5, 6), new Fraction(1, 2).plus(new Fraction(1, 3)));
+    }
+
+    @Test
+    public void reduceResultToWholeNumber() {
+        assertEquals(new Fraction(1), new Fraction(1, 3).plus(new Fraction(2, 3)));
+    }
+
+    @Test
+    public void oneDenominatorIsTheMultipleOfTheOther() {
+        assertEquals(new Fraction(11, 8), new Fraction(3, 4).plus(new Fraction(5, 8)));
+    }
+
+    @Test
+    public void commonFactorInDenominators() {
+        assertEquals(new Fraction(11, 18), new Fraction(1, 6).plus(new Fraction(4, 9)));
+    }
+
+    @Test
+    public void reduceResultEvenWhenDenominatorsAreTheSame() {
+        assertEquals(new Fraction(3, 2), new Fraction(3, 4).plus(new Fraction(3, 4)));
+    }
+
+    @Test
+    public void negativeFractionAndReducing() {
+        assertEquals(new Fraction(1, 2), new Fraction(-1, 4).plus(new Fraction(3, 4)));
+        assertEquals(new Fraction(-1, 8), new Fraction(3, 8).plus(new Fraction(-1, 2)));
+    }
+
+    @Test
+    @Ignore("Constructor doesn't yet avoid negative denominators")
+    public void negativeSignsEverywhere() {
+        assertEquals(new Fraction(1, 2), new Fraction(1, -4).plus(new Fraction(-3, -4)));
     }
 }
