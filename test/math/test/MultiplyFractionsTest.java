cd7b4eb0490e34fff3fd5f0bd1a1bbe7ad584306
Priscilla Romagnoli <priscillaromagnoli@hotmail.com>
2018-07-14 22:42:27 -0300

    Not zero times not zero

--- test/math/test/MultiplyFractionsTest.java
+++ test/math/test/MultiplyFractionsTest.java
@@ -20,4 +20,9 @@ public class MultiplyFractionsTest {
     public void zeroTimesNotZero() {
         assertEquals(new Fraction(0), new Fraction(0).times(new Fraction(5)));
     }
+
+    @Test
+    public void notZeroTimesNotZero() {
+        assertEquals(new Fraction(12), new Fraction(3).times(new Fraction(4)));
+    }
 }
