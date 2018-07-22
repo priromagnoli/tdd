7b45e9b2d03b4aff407e6ed614204f3af965cec6
Priscilla Romagnoli <priscillaromagnoli@hotmail.com>
2018-07-22 17:39:33 -0300

    Moved lcm to its class

--- src/math/NumberTheory.java
+++ src/math/NumberTheory.java
@@ -10,4 +10,11 @@ public class NumberTheory {
         }
         return Math.abs(a);
     }
+
+    public static int lcm(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return (a * b) / gcd(a, b);
+    }
 }
--- test/math/test/LeastCommonMultipleTest.java
+++ test/math/test/LeastCommonMultipleTest.java
@@ -2,20 +2,14 @@ package math.test;
 
 import org.junit.Test;
 
-import static math.NumberTheory.gcd;
+import static math.NumberTheory.lcm;
 import static org.junit.Assert.assertEquals;
 
 public class LeastCommonMultipleTest {
     @Test
-    public void name() {
+    public void resultOfMultiplication() {
         assertEquals(6, lcm(3, 2));
     }
 
-    public static int lcm(int a, int b) {
-        if (a == 0 || b == 0) {
-            return 0;
-        }
-        return (a * b) / gcd(a, b);
-    }
 
 }
